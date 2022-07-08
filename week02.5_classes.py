52e051edd5b0b7570d4d7891decb8d80e6e32fef
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-07-08 22:42:31 +0530

    Instance Variable and Class Variable

--- week02.5_classes.py
+++ week02.5_classes.py
@@ -1,6 +1,6 @@
 #Class
 
-'''
+#we create a class using Mobile as example
 class Mobile:
       def __init__(self):
             self.brandName = "Samsung"
@@ -31,7 +31,7 @@ def main():
       print(m2.brandName)
       print(m2.color)
       print(m2.isJack)
-      m2.calling'''
+      m2.calling
 
 #in the above as many mobiles we create everything will be same
 #so if we want mobiles to be from other manufactureres and diff colour
@@ -39,7 +39,7 @@ def main():
 #we can pass them as parametes and get them and print them 
 
 #eg
-'''
+
 class Mobile:
       def __init__(self,brandName,color,isJack):
             self.brandName = brandName
@@ -56,7 +56,7 @@ class Mobile:
             print("Message sent")
 
 
-#def main():
+def main():
 m1 = Mobile("Samsung","Mate Black",True)
 print(m1.brandName)
 print(m1.color)
@@ -70,10 +70,13 @@ m2= Mobile("Apple","Army Green",False)
 print(m2.brandName)
 print(m2.color)
 print(m2.isJack)
-m2.calling("Sanjay")'''
+m2.calling("Sanjay")
 
 
 #creating a class student
+
+from sqlalchemy import false, true
+
 
 class Student():
       def __init__(self,studentName,studentRegNo,studentclass):
@@ -86,3 +89,37 @@ student1 = Student("Dineshkumar","11711397","E1707")
 student2 = Student("Jey","11753463","M1703")
 print(student1.studentName, student1.studentRegNo,student1.studentClass)
 print(student2.studentName,student2.studentRegNo,student2.studentClass)
+
+
+
+#create class for showing 
+'''Instance Variable and Class Variable'''
+class Mobile:
+      tempered = true
+      chargingwithbox = false
+      #the above two are class variables and we can access with any instance
+
+      def __init__(self,brandName,colour,isJack):
+            self.brandName = brandName
+            self.colour = colour
+            self.isJack = isJack
+            #the above are instance variables these change for every value
+
+mobileM1 = Mobile("Oppo","Black",true)
+mobileM2 = Mobile("OnePlus","Blue",false)   
+print(mobileM1.brandName)
+print(mobileM2.colour)
+
+#we can also able to create a instance variable
+#eg:
+mobileM1.screen = "LED"
+print(mobileM1.screen)
+#now this will print LED but if we use for other instance that is M2 
+#now we will get error and this is why it is known as Instance variable
+
+#now will create a class variable
+Mobile.year ="2022"
+#now we can use this to any instance we create
+print(mobileM1.year)
+print(mobileM2.year)
+#i.e: the variables that are limited to certain instance are called Instance variable and to the class are called Class Variable
