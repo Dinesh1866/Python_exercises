f14de31cddf8d4e711a6fd368d376d39d82065b7
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-07-17 19:57:20 +0530

    input as array

--- week03.1.py
+++ week03.1.py
@@ -1,2 +1,15 @@
 '''Arrays'''
+#getting input as array or list
 
+# 1. we can use for loop
+arr = []
+for ele in input().split():
+      arr.append(int(ele))
+for i in range(len(arr)):
+      print(arr[i],end=" ")
+
+
+# 2. we can get it through map function
+arr2 = list(map(int,input().split()))
+for i in range(len(arr2)):
+      print(arr2[i],end=" ")
