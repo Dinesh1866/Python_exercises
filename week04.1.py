1cd047927c542fb84607dc1edc408043ae9a320e
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-07-19 18:34:25 +0530

    added array left rotation

---
+++ week04.1.py
@@ -0,0 +1,55 @@
+'''Array Problem Solving'''
+
+'''#rotating an array by k times
+def rotatingRight(arr,n):
+      temp = arr[n-1]
+      for i in range(n-1,0,-1):
+            arr[i] =arr[i-1]
+      arr[0]=temp
+      return arr
+
+def printArr(arr,n):
+      for i in range(n):
+            print(arr[i],end=" ")
+
+def main():
+      t = int(input())
+      while t>0:
+            n,k =map(int,input().split())
+            arr = list(map(int,input().split()))
+            for i in range(k):
+                  rotatingRight(arr,n)
+            printArr(arr,n)
+            print()
+            t -=1
+
+if __name__=='__main__':
+      main()
+#note this is just a basic method not the best one  
+'''
+
+#now will do it for left rotation
+def rotateLeft(arr,n):
+      temp = arr[0]
+      for i in range(0,n-1):
+            arr[i]=arr[i+1]
+      arr[n-1]=temp
+
+def printArray(arr,n):
+      for i in range(n):
+            print(arr[i],end=" ")
+      print()
+
+
+def main():
+      T = int(input())
+      while T>0:
+            n,k = map(int,input().split())
+            arr = list(map(int,input().split()))
+            for i in range(k):
+                  rotateLeft(arr,n)
+            printArray(arr,n)
+            T -=1
+
+if __name__ =="__main__":
+      main()
