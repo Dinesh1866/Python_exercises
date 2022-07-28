dc5e39534941ee9e76d90a30746206ab0916762b
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-07-28 20:20:49 +0530

    added upper and lower triangle problem

--- week04.2.py
+++ week04.2.py
@@ -40,3 +40,39 @@ for i in range(n):
       for j in range(m):
             print(mat4[i][j], end=" ")
       print()
+
+
+
+'''Lower and Upper Triangular Matrices'''
+#UpperTriangle
+def UpperTriangle(mat1,n,m):
+  for i in range(n):
+    for j in range(m):
+      if i>j:
+        print("0", end=" ")
+      else:
+        print(mat1[i][j],end=" ")
+    print()
+    
+    
+#LowerTriangle
+def LowerTriangle(mat1,n,m):
+  for i in range(n):
+    for j in range(m):
+      if i<j:
+        print("0", end=" ")
+      else:
+        print(mat1[i][j],end=" ")
+    print()
+
+
+def main():
+  n,m = map(int,input().split())
+  mat1 = []
+  for i in range(n):
+    mat1.append(list(map(int,input().split())))
+  LowerTriangle(mat1,n,m)
+  UpperTriangle(mat1,n,m)
+    
+if __name__ =="__main__":
+  main()
