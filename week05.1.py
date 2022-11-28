2299424a90481126b0422d0a9c1a51392c865cf6
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-11-28 10:05:01 +0530

    added strings

---
+++ week05.1.py
@@ -0,0 +1,25 @@
+# Strings
+
+'''slicing strings
+slicing is a way to get a part of a string
+slicing is done by using the index of the string
+slicing is done by using the colon operator
+slicing is done by using the start index and the end index
+''' 
+s="python"
+s[0:3] #gives "pyt"
+
+s="python"
+s[0:6] #gives "python"
+
+s="python"
+s[0:7] #gives "python"
+#slice operator is not giving an error if the index is out of range
+
+s="python"
+s[-1:-4] #gives ""
+
+s="python"
+s[-1:-4:-1] #gives "noh"
+
+
